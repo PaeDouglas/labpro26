516168fc0b995a9e908ab0c694784315d54631e5
PaeDouglas <douglasfabriciopaes@gmail.com>
2026-02-09 15:17:38 +0000

    fiz nada

---
+++ olamaria.py
@@ -0,0 +1,7 @@
+def diga_ola():
+    nome = input('Digite seu nome: ') 
+    print(f'Ola {nome}! seja bem vindo(a). ')
+    idade =int(input('porfavor me diga sua idade: '))
+    print(f'perfeito{nome}. voce tem {idade} anos. ')
+
+diga_ola()
