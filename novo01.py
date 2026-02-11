2924214751beeaed3261e3254c27bd3dd76a54eb
PaeDouglas <douglasfabriciopaes@gmail.com>
2026-02-11 15:22:13 +0000

    q1 - q5

---
+++ novo01.py
@@ -0,0 +1,33 @@
+def q1():
+    print('douglas')
+#q1
+def q2():
+   print(30*27)
+    
+#q2
+def q3():
+    soma = 5+8+12
+    media = soma/3
+    print(f'(5+8+12)/3 ={media}')
+
+#q3
+def q4():
+    num = int(input('digite um numero: '))
+    print(num)
+
+#q4
+def q5():
+    num1 = float(input('digite um numero: '))
+    num2 = float(input('digite um numero: '))
+    print(num1)
+    print(num2)
+#q5()
+
+def q6():
+    num= int(input('digite um numero inteiro: '))
+    print(f' O antecessor de:{num} é {num-1}')
+    print(f' O sucessor de:[num] é {num+1}')
+#q6()
+
+def q7():
+    
